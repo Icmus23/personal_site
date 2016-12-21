354792f9b41d9496d25d152fd1c9f19dde5c8d9d
Icmus23 <i.cmus@mail.ru>
2016-12-21 13:08:17 +0200

    translations

--- src/AppBundle/Controller/BlogController.php
+++ src/AppBundle/Controller/BlogController.php
@@ -5,8 +5,6 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use AppBundle\Entity\Post;
-use Symfony\Component\HttpFoundation\Response;
 
 class BlogController extends Controller
 {
